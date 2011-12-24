9e6281b7c486b17e95e5bd83e87791694ac0356a
Nick Howes <nick@nickhowes.co.uk>
2011-12-24 16:59:46 +0000

    First, terrible commit

---
+++ utils.js
@@ -0,0 +1,6 @@
+
+  window.requestAnimationFrame || (window.requestAnimationFrame = window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function(callback, element) {
+    return window.setTimeout(function() {
+      return callback(+new Date());
+    }, 1000 / 60);
+  });
